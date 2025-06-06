bc2daec895ef4801e93feb954d80415c17248b8e
Vinimoraes53 <viniciosmoraes53@gmail.com>
2025-06-05 21:06:02 -0300

    !

--- space/app/index.tsx
+++ space/app/index.tsx
@@ -1,10 +1,56 @@
+import { View } from 'react-native'
+
 import Logo from '@/componets/templates/logo'
+
 import Pagina from '@/componets/templates/Pagina'
 
+import Pergunta from '@/componets/questionario/Pergunta'
+
+import useQuestionario from '@/data/hooks/useQuestionario'
+
+import Resultado from '@/componets/questionario/Resultado'
+
+
 export default function Index() {
+
+    const { pergunta, concluido, pontuacao, totalDePerguntas, responder, reiniciar } =
+
+        useQuestionario()
+
+
     return (
+
         <Pagina>
-            <Logo />
+
+            <View style={{ gap: 40 }}>
+
+                <Logo />
+
+                {concluido ? (
+
+                    <Resultado
+
+                        pontuacao={pontuacao}
+
+                        totalDePerguntas={totalDePerguntas}
+
+                        reiniciar={reiniciar}
+
+                    />
+
+                ) : (
+
+                    <Pergunta pergunta={pergunta} opcaoSelecionada={responder} />
+
+                )}
+
+            </View>
+
         </Pagina>
+
     )
+
 }
+
+
+
